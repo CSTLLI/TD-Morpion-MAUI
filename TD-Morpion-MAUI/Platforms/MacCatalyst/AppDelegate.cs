747b5dfcdf57ec58220bf00ac6a730d50658d917
CASTELLI Valerio <78420708+CSTLLI@users.noreply.github.com>
2026-03-10 13:30:52 +0100

    fix: clean master

--- TD-Morpion-MAUI/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TD_Morpion_MAUI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
